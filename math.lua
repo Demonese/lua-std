44d0cb45690e2301a831019dc0ee756619219f1f
璀境石 <Kuanlan@outlook.com>
2024-03-01 16:06:38 +0800

    disable warning

--- math.lua
+++ math.lua
@@ -57,6 +57,8 @@ M.sin = math.sin
 M.tan = math.tan
 M.acos = math.acos
 M.asin = math.asin
+
+---@diagnostic disable-next-line: deprecated
 if math.atan2 then
     --- Returns the arc tangent of `y/x` (in radians).  
     ---@param y  number
@@ -66,6 +68,7 @@ if math.atan2 then
     ---@diagnostic disable-next-line: duplicate-set-field
     function M.atan(y, x)
         if x then
+            ---@diagnostic disable-next-line: deprecated
             return math.atan2(y, x)
         else
             return math.atan(y)
@@ -83,11 +86,14 @@ else
         return math.atan(y, x)
     end
 end
+
 M.sqrt = math.sqrt
 
 --- hyperbolic function
 
+---@diagnostic disable-next-line: deprecated
 if math.cosh then
+    ---@diagnostic disable-next-line: deprecated
     M.cosh = math.cosh
 else
     --- Returns the hyperbolic cosine of `x` (assumed to be in radians). 
@@ -99,7 +105,10 @@ else
         return (M.exp(x) + M.exp(-x)) / 2.0
     end
 end
+
+---@diagnostic disable-next-line: deprecated
 if math.sinh then
+    ---@diagnostic disable-next-line: deprecated
     M.sinh = math.sinh
 else
     --- Returns the hyperbolic sine of `x` (assumed to be in radians). 
@@ -111,7 +120,10 @@ else
         return (M.exp(x) - M.exp(-x)) / 2.0
     end
 end
+
+---@diagnostic disable-next-line: deprecated
 if math.tanh then
+    ---@diagnostic disable-next-line: deprecated
     M.tanh = math.tanh
 else
     --- Returns the hyperbolic tangent of `x` (assumed to be in radians).  
@@ -126,7 +138,9 @@ end
 
 --- power & exponential function
 
+---@diagnostic disable-next-line: deprecated
 if math.pow then
+    ---@diagnostic disable-next-line: deprecated
     M.pow = math.pow
 else
     --- Returns `x ^ y` .  
